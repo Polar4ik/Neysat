c951d508d3c0491411053690321d6b6ca595a3d5
Polar4ik <atossnikitoss@gmail.com>
2023-11-29 16:35:32 +0400

    Add: Roof detecter and inability to stand up if there is little space overhead

--- scripts/state machine/states/croach.gd
+++ scripts/state machine/states/croach.gd
@@ -1,12 +1,19 @@
 extends Idle
 
+var not_croach: bool = false
+
 func enter() -> void:
+	not_croach = false
+	
 	$"../../NormalCollision".disabled = true
 	$"../../CroachCollision".disabled = false
 
 func _unhandled_input(_event: InputEvent) -> void:
-	if Input.is_action_just_released("croach"):
+	if Input.is_action_just_released("croach") and not $"../../Head/RoofDetecter".is_colliding():
 		change.emit(self, "idle")
+	elif not Input.is_action_pressed("croach") and $"../../Head/RoofDetecter".is_colliding():
+		not_croach = true
+	
 	
 	var input_vec: Vector2 = Input.get_vector("left", "right", "forward", "back")
 	
@@ -16,4 +23,7 @@ func _unhandled_input(_event: InputEvent) -> void:
 
 func update(_delta: float) -> void:
 	$"../../Head".position.y = lerpf($"../../Head".position.y, 0.0, 0.5)
+	
+	if not_croach and not $"../../Head/RoofDetecter".is_colliding():
+		change.emit(self, "idle")
 
--- scripts/state machine/states/crochwalk.gd
+++ scripts/state machine/states/crochwalk.gd
@@ -2,19 +2,30 @@ extends Walk
 
 var CROACH_SPEED: float = 1.0
 
+var not_croach: bool = false
 
 func enter() -> void:
+	not_croach = false
+	
 	$"../../NormalCollision".disabled = true
 	$"../../CroachCollision".disabled = false
+	
 	speed = CROACH_SPEED
 
 func _unhandled_input(_event: InputEvent) -> void:
 	input_vec = Input.get_vector("left", "right", "forward", "back")
 	
-	if Input.is_action_just_released("croach"):
+	if not Input.is_action_pressed("croach") and not $"../../Head/RoofDetecter".is_colliding():
 		change.emit(self, "walk")
+	
+	elif not Input.is_action_pressed("croach") and $"../../Head/RoofDetecter".is_colliding():
+		not_croach = true
 
 func update(_delta: float) -> void:
+	if not_croach and not $"../../Head/RoofDetecter".is_colliding():
+		change.emit(self, "walk")
+	
 	$"../../Head".position.y = lerpf($"../../Head".position.y, 0.0, 0.5)
+	
 	if input_vec == Vector2.ZERO:
 		change.emit(self, "croach")
